659643d32e3a06af0366f06568b276e40181fdcd
Alexander Huber <alex.huber@alengo.at>
2025-05-24 00:25:43 +0200

    Twig WebspaceSettingsExtension & Readme

---
+++ Twig/WebspaceSettingsExtension.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alengo\Bundle\AlengoWebspaceSettingsBundle\Twig;
+
+use Alengo\Bundle\AlengoWebspaceSettingsBundle\Entity\WebspaceSettings;
+use Alengo\Bundle\AlengoWebspaceSettingsBundle\Repository\WebspaceSettingsRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class WebspaceSettingsExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly WebspaceSettingsRepository $repository,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('webspaceSettings', $this->webspaceSettings(...)),
+        ];
+    }
+
+    public function webspaceSettings($typeKey, $locale = false, $webspaceKey = false)
+    {
+        $requestWebspaceKey = $this->requestStack->getCurrentRequest()->attributes->get(key: '_sulu')->getAttributes()['webspace']->getKey();
+        if ($webspaceKey) {
+            $requestWebspaceKey = $webspaceKey;
+        }
+
+        if (!$requestWebspaceKey) {
+            return false;
+        }
+
+        $queryParameter = [
+            'typeKey' => $typeKey,
+            'webspaceKey' => $requestWebspaceKey,
+            'enabled' => true,
+        ];
+
+        if ($locale) {
+            $queryParameter['locale'] = $locale;
+        }
+
+        $webspaceSettings = $this->repository->findOneBy($queryParameter);
+
+        if (!$webspaceSettings instanceof WebspaceSettings) {
+            return false;
+        }
+
+        return $this->mapWebspaceSettingsData($webspaceSettings);
+    }
+
+    private function mapWebspaceSettingsData(WebspaceSettings $webspaceSettings): string|array|int|bool|null
+    {
+        return match ($webspaceSettings->getType()) {
+            'string', 'stringLocale', 'contact', 'media', 'organization' => $webspaceSettings->getData()[0] ?? false,
+            'medias' => $webspaceSettings->getData()['ids'] ?? false,
+            default => $webspaceSettings->getData(),
+        };
+    }
+}
